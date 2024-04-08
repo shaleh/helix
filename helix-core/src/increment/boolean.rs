4d64481c50769d833776927cefaf360305114364
Sean 'Shaleh' Perry <shalehperry@gmail.com>
2024-04-08 00:54:36 -0700

    refresh to current master

--- helix-core/src/increment/boolean.rs
+++ helix-core/src/increment/boolean.rs
@@ -1,80 +1,26 @@
-use super::Increment;
-use ropey::RopeSlice;
-
-use crate::{
-    textobject::{textobject_word, TextObject},
-    Range, Tendril,
-};
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct BooleanIncrementor<'a> {
-    incremented: &'a str,
-    range: Range,
-}
-
-impl<'a> BooleanIncrementor<'a> {
-    /// Return information about boolean under range if there is one.
-    pub fn from_range(text: RopeSlice, range: Range) -> Option<BooleanIncrementor> {
-        let range = textobject_word(text, range, TextObject::Inside, 1, false);
-        let word: String = text.slice(range.from()..range.to()).chars().collect();
-
-        let incremented = match word.as_str() {
-            "false" => "true",
-            "true" => "false",
-            "False" => "True",
-            "True" => "False",
-            "FALSE" => "TRUE",
-            "TRUE" => "FALSE",
-            _ => return None,
-        };
-
-        Some(BooleanIncrementor { incremented, range })
+/// Increment a boolean.
+///
+pub fn increment(selected_text: &str, _: i64) -> Option<String> {
+    if selected_text.is_empty() {
+        return None;
     }
-}
 
-impl<'a> Increment for BooleanIncrementor<'a> {
-    fn increment(&self, _amount: i64) -> (Range, Tendril) {
-        (self.range, self.incremented.into())
-    }
+    let incremented = match selected_text {
+        "false" => "true",
+        "true" => "false",
+        "False" => "True",
+        "True" => "False",
+        "FALSE" => "TRUE",
+        "TRUE" => "FALSE",
+        _ => return None,
+    };
+
+    Some(String::from(incremented))
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::Rope;
-
-    #[test]
-    fn test_boolean_from_range() {
-        let rope = Rope::from_str("Test text true more text.");
-        let range = Range::point(12);
-        assert_eq!(
-            BooleanIncrementor::from_range(rope.slice(..), range),
-            Some(BooleanIncrementor {
-                range: Range::new(10, 14),
-                incremented: "false",
-            })
-        );
-        let range = Range::point(10);
-        assert_eq!(
-            BooleanIncrementor::from_range(rope.slice(..), range),
-            Some(BooleanIncrementor {
-                range: Range::new(10, 14),
-                incremented: "false",
-            })
-        );
-        let range = Range::point(13);
-        assert_eq!(
-            BooleanIncrementor::from_range(rope.slice(..), range),
-            Some(BooleanIncrementor {
-                range: Range::new(10, 14),
-                incremented: "false",
-            })
-        );
-        let range = Range::point(14);
-        assert_eq!(BooleanIncrementor::from_range(rope.slice(..), range), None,);
-        let range = Range::point(9);
-        assert_eq!(BooleanIncrementor::from_range(rope.slice(..), range), None,);
-    }
 
     #[test]
     fn test_boolean_increment() {
@@ -88,15 +34,7 @@ mod test {
         ];
 
         for (original, expected) in tests {
-            let rope = Rope::from_str(original);
-            let range = Range::point(0);
-            assert_eq!(
-                BooleanIncrementor::from_range(rope.slice(..), range)
-                    .unwrap()
-                    .increment(1)
-                    .1,
-                Tendril::from(expected)
-            );
+            assert_eq!(increment(original, 0), Some(String::from(expected)));
         }
     }
 }
